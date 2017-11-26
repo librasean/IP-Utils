16b03565d43a64b77061c15cb9e1b7d53c2c204e
Sean <librasean@users.noreply.github.com>
2017-11-26 16:17:15 +0000

    feat(isPrivate): Added isPrivate IP check (#13)
    #8

--- spec/IpAddressesV4.constants.ts
+++ spec/IpAddressesV4.constants.ts
@@ -1,11 +1,11 @@
-export const VALID_IPS = [
+export const VALID_IPS: string[] = [
   '1.1.1.1',
   '255.255.255.255',
   '0.0.0.1',
   '192.168.1.1'
 ]
 
-export const INVALID_IPS = [
+export const INVALID_IPS: string[] = [
   'String',
   '-1.1.1.1',
   '256.1.1.1',
@@ -15,15 +15,26 @@ export const INVALID_IPS = [
   '500.2.1.4'
 ]
 
-export const VALID_IPS_NUMBER = [
+export const VALID_IPS_NUMBER: number[] = [
   16843009,
   4294967295,
   1,
   3232235777
 ]
 
-export const INVALID_IPS_NUMBER = [
+export const INVALID_IPS_NUMBER: number[] = [
   -1,
   4294967296,
   0
+]
+
+export const PRIVATE_IPS: (string | number)[] = [
+  '192.168.1.1',
+  3232235777
+]
+
+export const PUBLIC_IPS: (string | number)[] = [
+  '25.1.1.1',
+  '8.8.8.8',
+  '4.2.2.2'
 ]
