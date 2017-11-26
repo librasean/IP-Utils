8cebb2387081e631ec9d5c0451110201c9b1dc31
librasean <sanelson87@gmail.com>
2017-11-26 13:55:33 +0000

    feat(NumberToString): Added functionality for NumberToString ip conversion
    #1

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
 const IPV4 = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
 const IPV6 = /^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$/
 
-export function isValidIp (ipaddress: any) {
+export function isValidIp (ipaddress: any): Boolean {
   return (isValidIpv4(ipaddress) || isValidIpv6(ipaddress))
 }
 
-export function isValidIpv4 (ipaddress: any) {
+export function isValidIpv4 (ipaddress: any): Boolean {
   return IPV4.test(ipaddress)
 }
 
-export function isValidIpv6 (ipaddress: any) {
+export function isValidIpv6 (ipaddress: any): Boolean {
   return IPV6.test(ipaddress)
 }
 
-export function stringToInt (ipaddress: String) {
+export function stringToInt (ipaddress: String): Number {
   return ipaddress.split('.').reduce(function (ipInt, octet) {
     return (ipInt << 8) + parseInt(octet, 10)
   } , 0) >>> 0
+}
+
+export function intToSring (ipInt: number): String {
+  return ((ipInt >>> 24) + '.' + (ipInt >> 16 & 255) + '.' + (ipInt >> 8 & 255) + '.' + (ipInt & 255))
 }
