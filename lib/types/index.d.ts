3b04b7744b48643bccf1a9602386a1ae5049fe6f
librasean <sanelson87@gmail.com>
2017-11-27 20:49:04 +0000

    fix(Structure): Included build artifacts in SCM

---
+++ lib/types/index.d.ts
@@ -0,0 +1,6 @@
+export declare function isValidIp(ipaddress: any): Boolean;
+export declare function isValidIpv4(ipaddress: any): Boolean;
+export declare function isValidIpv6(ipaddress: any): Boolean;
+export declare function stringToInt(ipaddress: String): Number;
+export declare function intToSring(ipInt: number): String;
+export declare function isPrivate(ipaddress: string): Boolean;
