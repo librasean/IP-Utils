d722d3642aae217fe0be0e5273e43d76d5702670
librasean <sanelson87@gmail.com>
2017-11-27 18:25:53 +0000

    feat(GetOctet): Additional test for calling of GetOctetArray
    GetOCtet should first call GetOctetArray added a unit for this
    
    #10

--- spec/GetOctetArray.spec.ts
+++ spec/GetOctetArray.spec.ts
@@ -1,21 +1,29 @@
 import 'jasmine'
-import { getOctetArray } from '../src/index'
+import * as octetFunctions from '../src/index'
 import { VALID_IPS } from './TestingConstants'
 
-describe('getOctetArray function', () => {
-  it('Should return an Array when given a valid input', () => {
-    VALID_IPS.forEach(ip => expect(getOctetArray(ip)).toEqual(jasmine.any(Array)))
+describe('getOctet function', () => {
+  it('Should call getOctetArray first', () => {
+    let spy = spyOn(octetFunctions, 'getOctetArray')
+    octetFunctions.getOctet('1.1.1.1',1)
+    expect(spy).toHaveBeenCalled
   })
 
-  it('Should turn valid IPv4 into array of 4 things', () => {
-    VALID_IPS.forEach(ip => expect(getOctetArray(ip).length).toEqual(4))
-  })
+  describe('getOctetArray function', () => {
+    it('Should return an Array when given a valid input', () => {
+      VALID_IPS.forEach(ip => expect(octetFunctions.getOctetArray(ip)).toEqual(jasmine.any(Array)))
+    })
 
-  it('Should set correct order to octets from string IP', () => {
-    let resultArray = getOctetArray('1.2.3.4')
-    expect(resultArray[0]).toEqual('1')
-    expect(resultArray[1]).toEqual('2')
-    expect(resultArray[2]).toEqual('3')
-    expect(resultArray[3]).toEqual('4')
+    it('Should turn valid IPv4 into array of 4 things', () => {
+      VALID_IPS.forEach(ip => expect(octetFunctions.getOctetArray(ip).length).toEqual(4))
+    })
+
+    it('Should set correct order to octets from string IP', () => {
+      let resultArray = octetFunctions.getOctetArray('1.2.3.4')
+      expect(resultArray[0]).toEqual('1')
+      expect(resultArray[1]).toEqual('2')
+      expect(resultArray[2]).toEqual('3')
+      expect(resultArray[3]).toEqual('4')
+    })
   })
 })
